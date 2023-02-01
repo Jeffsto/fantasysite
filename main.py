20b5a8f01ac12e43b21189298852750d7c3b0d02
Jeff S <jeffsto@gmail.com>
2023-01-31 20:05:51 -0500

    Working on DB access

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request
 import datafile
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__, static_folder='/static')
 app.config['SECRET_KEY'] = datafile.storedkey
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{datafile.mysqladmin}:{datafile.mysql}@{datafile.mysqlhost}:3306/{datafile.mysqldb}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{datafile.mysqladmin}:{datafile.mysqlpw}@{datafile.mysqlhost}:3306/{datafile.mysqldb}'
+print(app.config['SQLALCHEMY_DATABASE_URI'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 @app.route("/")
 def index():
